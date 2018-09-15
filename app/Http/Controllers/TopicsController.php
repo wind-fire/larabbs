0f71948ccdd962f6eef2aae986ff0cec464b4dd3
wind-fire <877400859@qq.com>
2018-09-15 11:57:17 +0000

    User can create topic

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -2,16 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Topic;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
+use Illuminate\Support\Facades\Auth;
 
 class TopicsController extends Controller
 {
     public function __construct()
     {
         $this->middleware('auth', ['except' => ['index', 'show']]);
+//        $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
 /*	public function index()
@@ -29,19 +32,29 @@ class TopicsController extends Controller
 
     public function show(Topic $topic)
     {
+
         return view('topics.show', compact('topic'));
     }
 
 	public function create(Topic $topic)
 	{
-		return view('topics.create_and_edit', compact('topic'));
+        $categories = Category::all();
+		return view('topics.create_and_edit', compact('topic','categories'));
 	}
 
-	public function store(TopicRequest $request)
+	/*public function store(TopicRequest $request)
 	{
 		$topic = Topic::create($request->all());
 		return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
-	}
+	}*/
+    public function store(TopicRequest $request, Topic $topic)
+    {
+        $topic->fill($request->all());
+        $topic->user_id = Auth::id();
+        $topic->save();
+
+        return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
+    }
 
 	public function edit(Topic $topic)
 	{
