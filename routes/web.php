6fc8d9f9c8ae8cbadd114a7390b90b47b9fbc0f5
wind-fire <877400859@qq.com>
2018-09-26 02:08:20 +0000

    Setup default home page

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/','PagesController@root')->name('root');
+//Route::get('/','PagesController@root')->name('root');
+Route::get('/', 'TopicsController@index')->name('root');
 
 /*Auth::routes();
 
